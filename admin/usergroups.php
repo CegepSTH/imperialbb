f657de158f5fb69016afbc6d5476d50e048ec5c2
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-01-25 16:13:52 -0500

    Converted SQL queries to PDO. See issue #2 and issue #5.

--- admin/usergroups.php
+++ admin/usergroups.php
@@ -14,7 +14,7 @@ define("IN_IBB", 1);
 define("IN_ADMIN", 1);
 
 $root_path = "../";
-include($root_path . "includes/common.php");
+require_once($root_path . "includes/common.php");
 $language->add_file("admin/usergroups");
 
 if(!isset($_GET['func'])) $_GET['func'] = "";
@@ -24,8 +24,9 @@ if($_GET['func'] == "permissions")
 	if(!isset($_GET['id']) && !isset($_POST['id']))
 	{
 		$theme ->new_file("ug_select", "ug_select.tpl");
-		$sql = $db->query("SELECT * FROM `".$db_prefix."usergroups`");
-		while($result = $db->fetch_array($sql))
+		$db2->query("SELECT * FROM `_PREFIX_usergroups`");
+
+		while($result = $db2->fetch())
 		{
 			$theme->insert_nest("ug_select", "ug_select", array(
 				"GROUP_ID" => $result['id'],
@@ -52,28 +53,29 @@ if($_GET['func'] == "permissions")
 	{
 		if(isset($_POST['Submit']))
 		{
-			$sql = $db->query("SELECT * FROM `".$db_prefix."forums`");
-			while($result = $db->fetch_array($sql))
+			$db2->query("SELECT * FROM `_PREFIX_forums`");
+			
+			while($result = $db2->fetch())
 			{
 				$forum_id = $result['forum_id'];
 
 				if($_POST[$forum_id]['Read'] == "2" && $_POST[$forum_id]['Post'] == "2" && $_POST[$forum_id]['Reply'] == "2" && $_POST[$forum_id]['Poll'] == "2" && $_POST[$forum_id]['Create_Poll'] == "2" && $_POST[$forum_id]['Mod'] == "2")
 				{
-					$db->query("DELETE FROM `".$db_prefix."ug_auth` WHERE `usergroup` = '".$_GET['id']."' && `ug_forum_id` = '$forum_id'");#
+					$db2->query("DELETE FROM `_PREFIX_ug_auth` WHERE `usergroup` = '".$_GET['id']."' && `ug_forum_id` = '$forum_id'");#
 				}
 				else
 				{
 
-					$ug_sql = $db->query("SELECT * FROM `".$db_prefix."ug_auth` WHERE `usergroup` = '".$_GET['id']."' && `ug_forum_id` = '$forum_id'");
-					if(!$db->fetch_array($ug_sql))
+					$ug_sql = $db2->query("SELECT * FROM `_PREFIX_ug_auth` WHERE `usergroup` = '".$_GET['id']."' && `ug_forum_id` = '$forum_id'");
+					if(!$db2->fetch())
 					{
-						$db->query("INSERT INTO `".$db_prefix."ug_auth`
+						$db2->query("INSERT INTO `_PREFIX_ug_auth`
 									VALUES('".$_GET['id']."', '$forum_id', '".$_POST[$forum_id]['Read']."', '".$_POST[$forum_id]['Post']."', '".$_POST[$forum_id]['Reply']."', '".$_POST[$forum_id]['Create_Poll']."', '".$_POST[$forum_id]['Poll']."', '".$_POST[$forum_id]['Mod']."')");
 						}
 						else
 						{
 
-							$db->query("UPDATE `".$db_prefix."ug_auth`
+							$db2->query("UPDATE `_PREFIX_ug_auth`
 										SET `usergroup` = '".$_GET['id']."', `ug_forum_id` = '$forum_id', `ug_read` = '".$_POST[$forum_id]['Read']."', `ug_post` = '".$_POST[$forum_id]['Post']."', `ug_reply` = '".$_POST[$forum_id]['Reply']."', `ug_create_poll` = '".$_POST[$forum_id]['Create_Poll']."', `ug_poll` = '".$_POST[$forum_id]['Poll']."', `ug_mod` = '".$_POST[$forum_id]['Mod']."'
 										WHERE `usergroup` = '".$_GET['id']."' && `ug_forum_id` = '$forum_id'");
 						}
@@ -85,8 +87,8 @@ if($_GET['func'] == "permissions")
 				else
 				{
 					$theme->new_file("ug_auth", "ug_auth.tpl");
-					$sql = $db->query("SELECT * FROM `".$db_prefix."usergroups` WHERE `id` = '".$_POST['id']."'");
-					while($result = $db->fetch_array($sql))
+					$db2->query("SELECT * FROM `_PREFIX_usergroups` WHERE `id` = '".$_POST['id']."'");
+					while($result = $db2->fetch())
 					{
 						$theme->replace_tags("ug_auth", array(
 							"GROUP_ID" => $result['id'],
@@ -94,10 +96,11 @@ if($_GET['func'] == "permissions")
 						));
 					}
 
-					$sql = $db->query("SELECT * FROM `".$db_prefix."forums` ORDER BY `forum_id` DESC");
-					while($result = $db->fetch_array($sql)) {
-						$sql_auth = $db->query("SELECT * FROM `".$db_prefix."ug_auth` WHERE `usergroup` = '".$_POST['id']."' && `ug_forum_id` = '".$result['forum_id']."'");
-						if($result_auth = $db->fetch_array($sql_auth))
+					$pdo_forums = $db2->query("SELECT * FROM `_PREFIX_forums` ORDER BY `forum_id` DESC");
+					
+					while($result = $pdo_forums->fetch()) {
+						$db2->query("SELECT * FROM `_PREFIX_ug_auth` WHERE `usergroup` = '".$_POST['id']."' && `ug_forum_id` = '".$result['forum_id']."'");
+						if($result_auth = $db2->fetch())
 						{
 
 							$theme->insert_nest("ug_auth", "forum_permissions", array(
@@ -211,7 +214,7 @@ else if($_GET['func'] == "add")
 		}
 		else
 		{
-			$db->query("INSERT INTO `".$db_prefix."usergroups` (`name`, `desc`) VALUES ('".$_POST['name']."', '".$_POST['desc']."')");
+			$db2->query("INSERT INTO `_PREFIX_usergroups` (`name`, `desc`) VALUES ('".$_POST['name']."', '".$_POST['desc']."')");
 			info_box($lang['Create_Usergroup'], $lang['Usergroup_Created_Msg'], "usergroups.php");
 		}
 	}
@@ -279,14 +282,14 @@ else if($_GET['func'] == "add")
 		}
 		else
 		{
-			$db->query("UPDATE `".$db_prefix."usergroups` SET`name` = '".$_POST['name']."', `desc` = '".$_POST['desc']."' WHERE `id` = '".$_GET['id']."'");
+			$db2->query("UPDATE `_PREFIX_usergroups` SET`name` = '".$_POST['name']."', `desc` = '".$_POST['desc']."' WHERE `id` = '".$_GET['id']."'");
 			info_box($lang['Edit_Usergroup'], $lang['Usergroup_Updated_Msg'], "usergroups.php");
 		}
 	}
 	else
 	{
-		$sql = $db->query("SELECT * FROM `".$db_prefix."usergroups` WHERE `id` = '".$_GET['id']."'");
-		if($result = $db->fetch_array($sql))
+		$db2->query("SELECT * FROM `_PREFIX_usergroups` WHERE `id` = '".$_GET['id']."'");
+		if($result = $db2->fetch())
 		{
 			$theme->new_file("add_ug", "add_ug.tpl");
 			$theme->replace_tags("add_ug", array(
@@ -319,9 +322,9 @@ else if($_GET['func'] == "add")
 		if($_POST['new_ug'] != "-1") {
 			if(!isset($_POST['new_ug'])) error_msg($lang['Error'], $lang['Invalid_Usergroup_Id']);
 
-			$sql = $db->query("SELECT `id` FROM `".$db_prefix."usergroups` WHERE `id` = '".$_POST['new_ug']."'");
-			if($result = $db->fetch_array($sql)) {
-				$db->query("UPDATE `".$db_prefix."users` SET `user_usergroup` = '".$result['id']."' WHERE `user_usergroup` = '".$_GET['id']."'");
+			$db2->query("SELECT `id` FROM `_PREFIX_usergroups` WHERE `id` = '".$_POST['new_ug']."'");
+			if($result = $db2->fetch()) {
+				$db2->query("UPDATE `_PREFIX_users` SET `user_usergroup` = '".$result['id']."' WHERE `user_usergroup` = '".$_GET['id']."'");
 			}
 			else
 			{
@@ -330,19 +333,19 @@ else if($_GET['func'] == "add")
 		}
 		else
 		{
-			$db->query("UPDATE `".$db_prefix."users` SET `user_usergroup` = '0' WHERE `user_usergroup` = '".$_GET['id']."'");
+			$db2->query("UPDATE `_PREFIX_users` SET `user_usergroup` = '0' WHERE `user_usergroup` = '".$_GET['id']."'");
 		}
 
-		$db->query("DELETE FROM `".$db_prefix."usergroups` WHERE `id` = '".$_GET['id']."'");
-		$db->query("DELETE FROM `".$db_prefix."ug_auth` WHERE `usergroup` = '".$_GET['id']."'");
+		$db2->query("DELETE FROM `_PREFIX_usergroups` WHERE `id` = '".$_GET['id']."'");
+		$db2->query("DELETE FROM `_PREFIX_ug_auth` WHERE `usergroup` = '".$_GET['id']."'");
 		info_box($lang['Delete_Usergroup'], $lang['Usergroup_Deleted_Msg'], "usergroups.php");
-
 	}
 	else
 	{
 		$theme->new_file("delete_ug", "delete_ug.tpl");
-		$sql = $db->query("SELECT `name` FROM `".$db_prefix."usergroups` WHERE `id` = '".$_GET['id']."'");
-		if($result = $db->fetch_array($sql))
+		$db2->query("SELECT `name` FROM `_PREFIX_usergroups` WHERE `id` = '".$_GET['id']."'");
+		
+		if($result = $db->fetch())
 		{
 			$theme->replace_tags("delete_ug", array(
 				"UG_NAME" => $result['name']
@@ -353,8 +356,8 @@ else if($_GET['func'] == "add")
 			error_msg($lang['Error'], $lang['Invalid_Usergroup_Id']);
 		}
 
-		$sql = $db->query("SELECT `id`, `name` FROM `".$db_prefix."usergroups` WHERE `id` != '".$_GET['id']."'");
-		while($result = $db->fetch_array($sql))
+		$db2->query("SELECT `id`, `name` FROM `_PREFIX_usergroups` WHERE `id` != '".$_GET['id']."'");
+		while($result = $db2->fetch())
 		{
 			$theme->insert_nest("delete_ug", "ug_row", array(
 				"ID" => $result['id'],
@@ -381,8 +384,8 @@ else if($_GET['func'] == "add")
 else
 {
 	$theme->new_file("manage", "manage_ug.tpl");
-	$sql = $db->query("SELECT * FROM `".$db_prefix."usergroups`");
-	while($result = $db->fetch_array($sql)) {
+	$db2->query("SELECT * FROM `_PREFIX_usergroups`");
+	while($result = $db2->fetch()) {
 		$theme->insert_nest("manage", "ug_row", array(
 			"ID" => $result['id'],
 			"NAME" => $result['name'],
