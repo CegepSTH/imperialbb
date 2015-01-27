39571a0b1f4c5712ed77957ab1575a8ba647d28d
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-01-27 16:18:48 -0500

    Changed include() by require_once.

--- admin/menu.php
+++ admin/menu.php
@@ -14,7 +14,7 @@ define("IN_IBB", 1);
 define("IN_ADMIN", 1);
 
 $root_path = "../";
-include($root_path . "includes/common.php");
+require_once($root_path . "includes/common.php");
 
 /* Usage :
 "Block name" => array(
