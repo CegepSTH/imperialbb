c8627476b05f995c1821da61dc58de15c8fcb704
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-01-27 16:32:01 -0500

    Changes queries to fit issue #2 and issue #5. Also corrected a bug where the caracters where double escaped for the admin notepad.

--- admin/main.php
+++ admin/main.php
@@ -14,51 +14,46 @@ define("IN_IBB", 1);
 define("IN_ADMIN", 1);
 
 $root_path = "../";
-include($root_path."includes/common.php");
+require_once($root_path."includes/common.php");
 
-$config['admincp_notepad'] = htmlspecialchars($config['admincp_notepad']);
-if(isset($_GET['func']) && $_GET['func'] == 'update_notepad')
-{
-	$db->query("UPDATE `".$db_prefix."config` SET `config_value` = '".$db->escape_string(strip_tags(trim($_POST['admincp_notepad'])))."' WHERE `config_name` = 'admincp_notepad'");
+$config['admincp_notepad'] = $config['admincp_notepad'];
+if(isset($_GET['func']) && $_GET['func'] == 'update_notepad') {
+	$values = array(":admnote" => $_POST['admincp_notepad']);
+	$db2->query("UPDATE `_PREFIX_config` 
+		SET `config_value`=:admnote WHERE `config_name` = 'admincp_notepad'", $values);
 	info_box($lang['notepad_updated'], $lang['notepad_updated_desc'], "main.php");
 }
 
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."users` WHERE `user_id` > 0");
-if($result = $db->fetch_row($sql))
-{
-	$total_users = $result[0];
+$db2->query("SELECT count(*) AS `tot_users` FROM `_PREFIX_users` WHERE `user_id` > 0");
+if($result = $db2->fetch()) {
+	$total_users = $result['tot_users'];
 }
-$db->free($sql);
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."users` WHERE `user_date_joined` > '" . (time() - 86400) . "' AND `user_id` > 0");
-if($result = $db->fetch_row($sql))
-{
-	$users_today = $result[0];
+
+$db2->query("SELECT count(*) AS `nb_users` FROM `_PREFIX_users` WHERE `user_date_joined` > '".(time() - 86400)."' AND `user_id` > 0");
+if($result = $db2->fetch()) {
+	$users_today = $result['nb_users'];
 }
-$db->free($sql);
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."topics`");
-if($result = $db->fetch_row($sql))
-{
-	$total_topics = $result[0];
+
+$db2->query("SELECT count(*) AS `tot_topics` FROM `_PREFIX_topics`");
+if($result = $db2->fetch()) {
+	$total_topics = $result['tot_topics'];
 }
-$db->free($sql);
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."topics` WHERE `topic_time` > '" . (time() - 86400) . "'");
-if($result = $db->fetch_row($sql))
-{
-	$topics_today = $result[0];
+
+$db2->query("SELECT count(*) AS `today_topics` FROM `_PREFIX_topics` WHERE `topic_time` > '".(time() - 86400)."'");
+if($result = $db2->fetch()) {
+	$topics_today = $result['today_topics'];
 }
-$db->free($sql);
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."posts`");
-if($result = $db->fetch_row($sql))
-{
-	$total_posts = $result[0];
+
+$db2->query("SELECT count(*) AS `tot_posts` FROM `_PREFIX_posts`");
+if($result = $db2->fetch()) {
+	$total_posts = $result['tot_posts'];
 }
-$db->free($sql);
-$sql = $db->query("SELECT count(*) FROM `".$db_prefix."posts` WHERE `post_timestamp` > '" . (time() - 86400) . "'");
-if($result = $db->fetch_row($sql))
-{
-	$posts_today = $result[0];
+
+$db2->query("SELECT count(*) AS `posts_today` FROM `_PREFIX_posts` WHERE `post_timestamp` > '".(time() - 86400)."'");
+if($result = $db2->fetch()) {
+	$posts_today = $result['posts_today'];
 }
-$db->free($sql);
+
 $theme->new_file("main", "main.tpl", "");
 $theme->replace_tags("main", array(
 	"TOTAL_USERS"   => $total_users,
@@ -69,51 +64,18 @@ $theme->replace_tags("main", array(
 	"TOPICS_TODAY"  => $topics_today
 ));
 
-/* DEACTIVATED --Alexandre Leblanc
- * Super dupper dangerous. !!!
-$vcheck = file_get_contents("http://www.imperialbb.com/scripts/upgrades/");
-eval($vcheck);
-if($config['version'] < $latest_version)
-{
-	$upgrade_info = "";
-	foreach($upgrade_notes as $title => $upgrade_note)
-    {
-		if(is_array($upgrade_note))
-        {
-			$upgrade_info .= "$title:<br />";
-			foreach($upgrade_note as $upgrade_temp)
-            {
-				$upgrade_info .= " - $upgrade_temp<br />";
-			}
-			$upgrade_info .= "<br />";
-		}
-        else
-        {
-			$upgrade_info .= "$title<br /><br />";
-		}
-	}
-	$theme->switch_nest("main", "vcheck", false, array(
-		"INSTALL_NOTES" => $upgrade_info
-	));
-}
-else
-{
-	$theme->switch_nest("main", "vcheck", true);
-}*/
-
 $theme->add_nest("main", "vcheck");
-if(is_dir("../install"))
-{
-        $theme->insert_nest("main", "install_warning");
-        $theme->add_nest("main", "install_warning");
+if(is_dir("../install")) {
+	$theme->insert_nest("main", "install_warning");
+	$theme->add_nest("main", "install_warning");
 }
 
 // Output the page header
-include($root_path . "includes/page_header.php");
+include_once($root_path . "includes/page_header.php");
 
 // Output the main page
 $theme->output("main");
 
 // Output the page footer
-include($root_path . "includes/page_footer.php");
+include_once($root_path . "includes/page_footer.php");
 ?>
