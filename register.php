456162dbecf8f73a134bafdd77710cbf23006284
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-01-28 14:42:22 -0500

    Now can register when no email needed;

--- register.php
+++ register.php
@@ -19,50 +19,65 @@ define("IN_IBB", 1);
 $root_path = "./";
 require_once($root_path . "includes/common.php");
 require_once($root_path . "classes/password.php");
+require_once($root_path . "models/user.php");
 
 $language->add_file("register");
 
 if(isset($_POST['Submit'])) {
 	$error = "";
+	
 	if(strlen($_POST['UserName']) < 2) {
 		$error .= $lang['Username_Too_Short'] . "<br />";
 	} else if(userexists($_POST['UserName']) == 1) {
 		$error .= $lang['Username_Already_Taken'] . "<br />";
 	}
+	
 	if(strlen($_POST['Password']) < 4) {
 		$error .= $lang['Password_Too_Short'] . "<br />";
 	} else if($_POST['Password'] != $_POST['Pass2']) {
 		$error .= $lang['Passwords_Dont_Match'] . "<br />";
 	}
+	
 	if(!preg_match("#(.*?)@(.*?)\.(.*?)#", $_POST['Email'])) {
 		$error .= $lang['Invalid_Email_Address'] . "<br />";
 	}
+	
 	if(strlen($error) > 0) {
 		$theme->new_file("register", "register.tpl", "");
+		
 		$theme->replace_tags("register", array(
 			"USERNAME" => $_POST['UserName'],
 			"EMAIL" => $_POST['Email']
 		));
+		
 		$theme->insert_nest("register", "error", array(
 			"ERRORS" => $error
 		));
+		
 		$theme->add_nest("register", "error");
-	//
-	// Output the page header
-	//
-	include_once($root_path . "includes/page_header.php");
+		//
+		// Output the page header
+		//
+		include_once($root_path . "includes/page_header.php");
 
-	//
-	// Output the main page
-	//
-	$theme->output("register");
+		//
+		// Output the main page
+		//
+		$theme->output("register");
 
-	//
-	// Output the page footer
-	//
-	include_once($root_path . "includes/page_footer.php");
+		//
+		// Output the page footer
+		//
+		include_once($root_path . "includes/page_footer.php");
 	} else {
 		if($config['register_auth_type'] == 0) {
+			// No activation key.
+			$oUser = new User(-1, $_POST['UserName'], $_POST['Email']);
+			$oUser->setPassword($_POST['Password']);
+			$oUser->setLanguageId($config['default_template']);
+			$oUser->setTemplateId($config['default_language']);
+			$oUser->update();
+			/*
 			$db2->query("INSERT INTO `_PREFIX_users` (
 				`username`,
 				`user_password`,
@@ -83,17 +98,23 @@ if(isset($_POST['Submit'])) {
 				)",
 				array(
 					":username" => $_POST['UserName'],
-//					":user_password" => md5(md5($_POST['Password'])),
 					":user_password" => password_hash($_POST['Password'], PASSWORD_BCRYPT),
 					":user_email" => $_POST['Email'],
 					":user_date_joined" => date("D d M Y"),
 					":user_template" => $config['default_template'],
 					":user_language" => $config['default_language']
 				)
-			);
+			); */
 			info_box($lang['Registration'], $lang['Registration_Successful_Msg'], "?act=login");
 		} else {
+			$oUser = new User(-1, $_POST['UserName'], $_POST['Email']);
+			$oUser->setPassword($_POST['Password']);
+			$oUser->setLanguageId($config['default_template']);
+			$oUser->setTemplateId($config['default_language']);
 			$activation_key = generate_activate_key();
+			$oUser->setActivationKey($activation_key);
+			$oUser->update(true);
+			/*
 			$db2->query("INSERT INTO `_PREFIX_users` (
 				`username`,
 				`user_password`,
@@ -109,14 +130,13 @@ if(isset($_POST['Submit'])) {
 				:user_password,
 				:user_email,
 				:user_date_joined,
-				'3',
+				'2',
 				:user_activation_key,
 				:user_template,
 				:user_language
 				)",
 				array(
 					":username" => $_POST['UserName'],
-//					":user_password" => md5(md5($_POST['Password'])),
 					":user_password" => password_hash($_POST['Password'], PASSWORD_BCRYPT),
 					":user_email" => $_POST['Email'],
 					":user_date_joined" => date("D d M Y"),
@@ -124,8 +144,15 @@ if(isset($_POST['Submit'])) {
 					":user_template" => $config['default_template'],
 					":user_language" => $config['default_language']
 				)
-			);
-			email($lang['Email_New_Account_Subject'], "new_account", array("USER_ID" => $db2->lastInsertId(), "USERNAME" => $_POST['UserName'], "PASSWORD" => $_POST['Password'], "KEY" => $activation_key, "DOMAIN" => $config['url'], "SITE_NAME" => $config['site_name']), $_POST['Email']);
+			); */
+			email($lang['Email_New_Account_Subject'], "new_account", 
+				array("USER_ID" => $db2->lastInsertId(), 
+					"USERNAME" => $_POST['UserName'], 
+					"PASSWORD" => $_POST['Password'], 
+					"KEY" => $activation_key, 
+					"DOMAIN" => $config['url'], 
+					"SITE_NAME" => $config['site_name']), $_POST['Email']);
+					
 			info_box($lang['Registration'], $lang['Activate_Your_Acct_Msg'], "?act=login");
 		}
 	}
