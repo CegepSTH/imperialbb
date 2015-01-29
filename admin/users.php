bddb6ba30f1c07a916c3aa7b27b42cf4d9109af0
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-01-29 11:49:41 -0500

    Now uses entirely User model. See issue #6 for details.

--- admin/users.php
+++ admin/users.php
@@ -15,6 +15,7 @@ define("IN_ADMIN", 1);
 
 $root_path = "../";
 require_once($root_path."includes/common.php");
+require_once($root_path."models/user.php");
 
 if(!isset($_GET['func'])) $_GET['func'] = "";
 
@@ -22,95 +23,80 @@ if($_GET['func'] == "edit")
 {
 	if(isset($_POST['username']))
 	{
-		$db2->query("SELECT * FROM _PREFIX_users WHERE `username`=:uname LIMIT 1", array(":uname" => $_POST['username']));
-
-		if($result = $db2->fetch())
-		{
-			$theme->new_file("edit_user", "edit_user.tpl");
-			$theme->replace_tags("edit_user", array(
-				"USER_ID" => $result['user_id'],
-				"USERNAME" => $result['username'],
-				"EMAIL" => $result['user_email'],
-				"SIGNATURE" => $result['user_signature'],
-				"AIM" => $result['user_aim'],
-				"ICQ" => $result['user_icq'],
-				"MSN" => $result['user_msn'],
-				"YAHOO" => $result['user_yahoo']
-			));
-
-			$db2->query("SELECT * FROM _PREFIX_usergroups");
-
-			while($ug_result = $db2->fetch())
-			{
-				$theme->insert_nest("edit_user", "usergroup_option", array(
-					"UG_ID" => $ug_result['id'],
-					"UG_NAME" => $ug_result['name'],
-					"UG_SELECTED" => ($ug_result['id'] == $result['user_usergroup']) ? "selected=\"SELECTED\"" : ""
-				));
-				$theme->add_nest("edit_user", "usergroup_option");
-			}
-			
-			$db2->query("SELECT * FROM `_PREFIX_ranks`");
-	
-			while($rank_result = $db2->fetch())
-			{
-				$theme->insert_nest("edit_user", "rank_option", array(
-					"RANK_ID" => $rank_result['rank_id'],
-					"RANK_NAME" => $rank_result['rank_name'],
-					"RANK_SELECTED" => ($rank_result['rank_id'] == $result['user_rank']) ? "selected=\"SELECTED\"" : ""
-				));
-				$theme->add_nest("edit_user", "rank_option");
-			}
-			
-			$user_levels = array($lang['Administrator'] => "5", $lang['Moderator'] => "4", $lang['Registered'] => "3", $lang['Validating'] => "2", $lang['Guest'] => "1", $lang['Banned'] => "0");
-			foreach($user_levels as $ul_name => $ul_id)
-			{
-				$theme->insert_nest("edit_user", "user_level_option", array(
-					"UL_ID" => $ul_id,
-					"UL_NAME" => $ul_name,
-					"UL_SELECTED" => ($ul_id == $result['user_level']) ? "SELECTED" : ""
-				));
-				$theme->add_nest("edit_user", "user_level_option");
-			}
-
-			//
-			// Output the page header
-			//
-			include($root_path . "includes/page_header.php");
-
-			//
-			// Output the main page
-			//
-			$theme->output("edit_user");
-
-			//
-			// Output the page footer
-			//
-			include($root_path . "includes/page_footer.php");
-
-		}
-		else
-		{
+		// Fetch user.
+		$oUser = User::findUser(trim($_POST['username']));
+		
+		if($oUser == null || is_null($oUser)) {
 			error_msg($lang['Error'], $lang['Invalid_User_Id']);
+			exit();
 		}
+
+		$ims = $oUser->getMessengers();
+		$theme->new_file("edit_user", "edit_user.tpl");
+		$theme->replace_tags("edit_user", array(
+			"USER_ID" => $oUser->getId(),
+			"USERNAME" => $oUser->getUsername(),
+			"EMAIL" => $oUser->getEmail(),
+			"SIGNATURE" => $oUser->getSignature(),
+			"AIM" => $ims["aim"],
+			"ICQ" => $ims["icq"],
+			"MSN" => $ims["msn"],
+			"YAHOO" => $ims["yahoo"]
+		));
+		
+		// Fetch usergroups.
+		$db2->query("SELECT * FROM _PREFIX_usergroups");
+		while($ug_result = $db2->fetch()) {
+			$theme->insert_nest("edit_user", "usergroup_option", array(
+				"UG_ID" => $ug_result['id'],
+				"UG_NAME" => $ug_result['name'],
+				"UG_SELECTED" => ($ug_result['id'] == $oUser->getUsergroupId()) ? "selected=\"SELECTED\"" : ""
+			));
+			$theme->add_nest("edit_user", "usergroup_option");
+		}
+		
+		// Fetch ranks.		
+		$db2->query("SELECT * FROM `_PREFIX_ranks`");
+		while($rank_result = $db2->fetch()) {
+			$theme->insert_nest("edit_user", "rank_option", array(
+				"RANK_ID" => $rank_result['rank_id'],
+				"RANK_NAME" => $rank_result['rank_name'],
+				"RANK_SELECTED" => ($rank_result['rank_id'] == $oUser->getRankId()) ? "selected=\"SELECTED\"" : ""
+			));
+			$theme->add_nest("edit_user", "rank_option");
+		}
+		
+		// Fetch user levels.
+		$user_levels = array($lang['Administrator'] => "5", $lang['Moderator'] => "4", $lang['Registered'] => "3", $lang['Validating'] => "2", $lang['Guest'] => "1", $lang['Banned'] => "0");
+		foreach($user_levels as $ul_name => $ul_id)	{
+			$theme->insert_nest("edit_user", "user_level_option", array(
+				"UL_ID" => $ul_id,
+				"UL_NAME" => $ul_name,
+				"UL_SELECTED" => ($ul_id == $oUser->getLevel()) ? "SELECTED" : ""
+			));
+			
+			$theme->add_nest("edit_user", "user_level_option");
+		}
+		
+		include_once($root_path . "includes/page_header.php");
+		$theme->output("edit_user");
+		include_once($root_path . "includes/page_footer.php");
 	}
 	else if(isset($_GET['user_id']))
 	{
+		// Edit user.
 		$error = "";
-		if(empty($_POST['Username']))
-		{
+		if(empty($_POST['Username'])) {
 			$error .= sprintf($lang['No_x_content'], strtolower($lang['Username'])) . "<br />";
 		}
-		if(strlen($_POST['PassWord']) > 4 && strlen($_POST['PassWord']) < 0)
-		{
+		
+		if(strlen($_POST['PassWord']) > 4 && strlen($_POST['PassWord']) < 0) {
 			$error .= $lang['Password_Too_Short'] . "<br />";
-		}
-		else if($_POST['PassWord'] != $_POST['Pass2'])
-		{
+		} else if($_POST['PassWord'] != $_POST['Pass2']) {
 			$error .= $lang['Passwords_Dont_Match'] . "<br />";
 		}
-		if(!preg_match("#(.*?)@(.*?).(.*?)#", $_POST['Email']))
-		{
+		
+		if(!preg_match("#(.*?)@(.*?).(.*?)#", $_POST['Email'])) {
 			$error .= $lang['Invalid_Email_Address'] . "<br />";
 		}
 
@@ -128,10 +114,9 @@ if($_GET['func'] == "edit")
 				"YAHOO" => $_POST['yahoo']
 			));
 			
+			// Fetch usergroups.
 			$db2->query("SELECT * FROM `_PREFIX_usergroups`");
-			
-			while($ug_result = $db2->fetch())
-			{
+			while($ug_result = $db2->fetch()) {
 				$theme->insert_nest("edit_user", "usergroup_option", array(
 					"UG_ID" => $ug_result['id'],
 					"UG_NAME" => $ug_result['name'],
@@ -140,10 +125,9 @@ if($_GET['func'] == "edit")
 				$theme->add_nest("edit_user", "usergroup_option");
 			}
 			
+			// Fetch
 			$db2->query("SELECT * FROM `_PREFIX_ranks`");
-			
-			while($rank_result = $db2->fetch())
-			{
+			while($rank_result = $db2->fetch()) {
 				$theme->insert_nest("edit_user", "rank_option", array(
 					"RANK_ID" => $rank_result['rank_id'],
 					"RANK_NAME" => $rank_result['rank_name'],
@@ -152,9 +136,9 @@ if($_GET['func'] == "edit")
 				$theme->add_nest("edit_user", "rank_option");
 			}
 
+			// Fetch level.
 			$user_levels = array($lang['Administrator'] => "5", $lang['Moderator'] => "4", $lang['Registered'] => "3", $lang['Validating'] => "2", $lang['Guest'] => "1", $lang['Banned'] => "0");
-			foreach($user_levels as $ul_name => $ul_id)
-			{
+			foreach($user_levels as $ul_name => $ul_id) {
 				$theme->insert_nest("edit_user", "user_level_option", array(
 					"UL_ID" => $ul_id,
 					"UL_NAME" => $ul_name,
@@ -167,41 +151,30 @@ if($_GET['func'] == "edit")
 			));
 			$theme->add_nest("edit_user", "error");
 
-			//
-			// Output the page header
-			//
-			include($root_path . "includes/page_header.php");
-
-			//
-			// Output the main page
-			//
+			include_once($root_path . "includes/page_header.php");
 			$theme->output("edit_user");
-
-			//
-			// Output the page footer
-			//
-			include($root_path . "includes/page_footer.php");
-
+			include_once($root_path . "includes/page_footer.php");
 		}
 		else
 		{
-			$values = array(":uname" => $_POST['Username'], ":uemail" => $_POST['Email'], ":usign" => $_POST['signature'], 
-			":uaim" => $_POST['aim'], ":uicq" => $_POST['icq'], ":umsn" => $_POST['msn'], ":uyahoo" => $_POST['yahoo'],
-			":ugroup" => $_POST['usergroup'], ":urank" => $_POST['rank'], ":ulevel" => $_POST['user_level'], ":uid" => $_GET['user_id']);
+			$oUser = User::findUser($_GET['user_id']);
 			
-			$sql = "UPDATE `_PREFIX_users` SET `username`=:uname, `user_email`=:uemail, `user_signature`=:usign";
-
+			$oUser->setUsername($_POST['Username']); 
+			$oUser->setMail($_POST['Email']);
+			$oUser->setSignature($_POST['signature']);
+			$oUser->setMessengers(array("aim" => $_POST['aim'], "icq" => $_POST['icq'], "msn" => $_POST['msn'], "yahoo" => $_POST['yahoo']));
+			$oUser->setUsergroupId($_POST['usergroup']);
+			$oUser->setRankId($_POST['rank']);
+			$oUser->setLevel($_POST['user_level']); 
+			
+			$pass_ok = true;
 			if(strlen($_POST['PassWord']) > 0) {
-				$values[":upasswd"] = md5(md5($_POST['PassWord']));
-				$sql .= ", `user_password`=:upasswd";
+				$oUser->setPassword($_POST['PassWord']); 
+				$pass_ok = $oUser->updatePassword();
 			}
 			
-			$sql .= ", `user_aim`=:uaim, `user_icq`=:uicq, `user_msn`=:umsn, `user_yahoo`=:uyahoo, `user_usergroup`=:ugroup, `user_rank`=:urank, `user_level`=:ulevel
-			WHERE `user_id`=:uid";
-			
-			$db2->query($sql, $values);
-			
-			if($db2->getError() == "" || $db2->getError() == null) {
+			$ok = $oUser->update();
+			if($ok && $pass_ok) {
 				info_box($lang['Edit_User'], $lang['User_Updated_Msg'], "main.php");
 			}
 		}
@@ -210,28 +183,17 @@ if($_GET['func'] == "edit")
 	{
 		$theme->new_file("edit_user", "edit_user_start.tpl");
 		$db2->query("SELECT `username` FROM `_PREFIX_users` WHERE `user_id` > 0 ORDER BY `user_id` DESC LIMIT 50");
-		while($result = $db2->fetch())
-		{
+		
+		while($result = $db2->fetch()) {
 			$theme->insert_nest("edit_user", "user_option", array(
 				"USERNAME" => $result['username']
 			));
 			$theme->add_nest("edit_user", "user_option");
 		}
 		
-		//
-		// Output the page header
-		//
-		include($root_path . "includes/page_header.php");
-
-		//
-		// Output the main page
-		//
+		include_once($root_path . "includes/page_header.php");
 		$theme->output("edit_user");
-
-		//
-		// Output the page footer
-		//
-		include($root_path . "includes/page_footer.php");
+		include_once($root_path . "includes/page_footer.php");
 	}
 
 }
@@ -241,27 +203,17 @@ else if($_GET['func'] == "delete")
 	{
 		$theme->new_file("delete_user", "delete_user.tpl");
 		$db2->query("SELECT `username` FROM `_PREFIX_users` WHERE `user_id` > 0 ORDER BY `user_id` DESC LIMIT 25");
-		while($result = $db2->fetch())
-		{
+		
+		while($result = $db2->fetch()) {
 			$theme->insert_nest("delete_user", "user_option", array(
 				"USERNAME" => $result['username']
 			));
 			$theme->add_nest("delete_user", "user_option");
 		}
-		//
-		// Output the page header
-		//
-		include($root_path . "includes/page_header.php");
 
-		//
-		// Output the main page
-		//
+		include_once($root_path . "includes/page_header.php");
 		$theme->output("delete_user");
-
-		//
-		// Output the page footer
-		//
-		include($root_path . "includes/page_footer.php");
+		include_once($root_path . "includes/page_footer.php");
 	}
 	else if(!isset($_POST['confirm']) || $_POST['confirm'] != "Yes")
 	{
@@ -269,7 +221,7 @@ else if($_GET['func'] == "delete")
 	}
 	else
 	{
-		$db2->query("DELETE FROM `_PREFIX_users` WHERE `username`=:uname LIMIT 1", array(":uname" => $_GET['username']));
+		User::delete($_GET['username']);
 		info_box($lang['Delete_User'], $lang['User_Deleted_Msg'], "main.php");
 	}
 }
