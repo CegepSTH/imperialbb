75d6f848e17ae7b3d75c1ca5dcf6e0bfd0b16c93
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-02-04 09:39:17 -0500

    Added render() function Pretty much finished the class.

--- classes/template.php
+++ classes/template.php
@@ -5,6 +5,8 @@ class Template {
 	private static $m_basePath;
 	// Associative array as $varName => $value
 	private $m_vars;
+	// Associative array as $tagName => $content
+	private $m_tags;
 	// Relative file path for the template.
 	private $m_filePath; 
 	
@@ -15,6 +17,7 @@ class Template {
 	 */
 	public __construct($str_file) {
 		$this->m_vars = array();
+		$this->m_tags = array();
 	}
 	
 	/**
@@ -52,7 +55,51 @@ class Template {
 			$content = $str_content->render();
 		}
 		
+		$this->m_tags[$str_tagName] .= $content;
+	}
+	
+	/**
+	 * Add multiple tags to template.
+	 * 
+	 * @param $tags Associative array as $name => $content
+	 * @note $content may be either a template object or a string.
+	 */
+	public addToTags(array $tags) {
+		foreach($tags as $name => $content) {
+			addToTag($name, $content);
+		}
+	}
+	
+	/**
+	 * Renders the template
+	 * 
+	 * @returns Parsed content string.
+	 */
+	public render() {
+		$content = "";
 		
+		$fPath = $this->m_basePath . "/" . $this->m_filePath;
+		$hFile = fopen($fPath, "r");
+		
+		if ($hFile) {
+			while (($sLine = fgets($hFile)) !== false) {
+				$sLineCopy = "";
+				
+				foreach($this->m_vars as $name => $value) {
+					$sLineCopy .= str_replace("{".$name."}", $value, $sLine);
+				}
+				
+				foreach($this->m_tags as $name => $value) {
+					$content .= str_replace("<!-- TAG ".$name." -->", $value, $sLineCopy);
+				}
+			}
+
+			fclose($hFile);
+		} else {
+			die(__FILE__ . " : Cannot open " . $fPath );
+		} 
+		
+		return $content;
 	}
 	
 	/**
