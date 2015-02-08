80b15bb9370878c7592b66dc6e5e5d7db0f1d9c5
Alexandre Leblanc <alex.cs00@yahoo.ca>
2015-02-08 15:56:41 -0500

    Added error codes.

--- includes/constants.php
+++ includes/constants.php
@@ -28,6 +28,15 @@ define("NO_AVATAR", 0);
 define("UPLOADED_AVATAR", 1);
 define("REMOTE_AVATAR", 2);
 
+//ERRORS
+define("ERR_CODE_USERNAME_NOT_SET", 10);
+define("ERR_CODE_USER_NOT_FOUND", 20);
+define("ERR_CODE_USER_CANT_UPDATE", 30);
+define("ERR_CODE_USER_CANT_DELETE", 40);
+define("ERR_CODE_USER_UPDATE_SUCCESS", 50);
+define("ERR_CODE_USER_DELETE_SUCCESS", 60);
+define("ERR_CODE_USER_PASS_MISMATCH", 70);
+
 /*======================================================================*\
 || #################################################################### ||
 || #                 "Copyright © 2006 M-ka Network"                  # ||
